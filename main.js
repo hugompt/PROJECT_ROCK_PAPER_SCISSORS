8d65bf3a1a6df97477ccbc2de4da8a18221918c6
hugompt <hugomsfh@hotmail.com>
2022-02-23 03:44:44 +0100

    3#UPDATE | 23/02/2022         Done:             - Added code to the main.js file;                 - Created global variables;                 - Added a return value for the playRound() function to be able to get a total score;                 - Wrote a function called game(). Call the playRound() function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end;             - Added a console.log command to view the winner of each round;
            ToDo:
                - Add code to the style.css file;
                - Add code to the index.html file;

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
+//Global variables
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = " ";
+
 //Function for randomly choosing a computer play between 3 possible options, Rock, Paper, Scissors;
-//Decided to write it on console and return it;
     function computerPlay (){
         const strMoves = ["Rock","Paper","Scissors"];
         return (strMoves[Math.floor(Math.random() * 3)]);
     }
 
-//Function to play a single round of Rock Paper Scissors;
-//2 Parameters, playerSelection and computerSelection;
+//Function to play a single round of Rock Paper Scissors VS the CPU;
     function playRound (playerSelection, computerSelection){
         playerSelection = playerSelection.toUpperCase();
         computerSelection = computerSelection.toUpperCase();
         if(playerSelection === computerSelection){
             alert("DRAW! Close but not enough! Both you and the CPU picked " + playerSelection);
+            return "DRAW"
         }else if (playerSelection === "ROCK" && computerSelection === "SCISSORS" || playerSelection === "PAPER" && computerSelection === "ROCK" || playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-            alert("You WIN!!! Your " + playerSelection + " beats CPU's " + computerSelection +"!");
+            alert("YOU WIN!!! Your " + playerSelection + " beats CPU's " + computerSelection +"!");
+            return "HUMAN"
         }else if (playerSelection === "ROCK" && computerSelection === "PAPER" || playerSelection === "PAPER" && computerSelection === "SCISSORS" || playerSelection === "SCISSORS" && computerSelection === "ROCK"){
-            alert("You LOSE! The CPU's " + computerSelection + " beats your " + playerSelection + ", try again!");
+            alert("YOU LOSE! The CPU's " + computerSelection + " beats your " + playerSelection + ", try again!");
+            return "CPU"
+        }else{
+            alert("WARNING! Please enter a valid response: 'Rock' or 'Paper' or 'Scissors' || not case-sensitive, write as you want :)")
+            return "INVALID_ROUND!"
+        }
+    }
+
+//Function to play a full game with 5 rounds VS the CPU
+    function game (){
+        for (let i = 0; i < 5; i++){
+            roundWinner = playRound(prompt("CHOOSE: Rock / Paper / Scissors"),computerPlay());
+            if ( roundWinner === "HUMAN"){
+                playerScore++;
+            }else if (roundWinner === "CPU"){
+                computerScore++;
+            }
+            console.log("The winner of this round is: " + roundWinner);
+        }
+        if (playerScore === computerScore){
+            alert("DRAW! Both YOU and the CPU scored: " + playerScore);
+        }else if(playerScore > computerScore){
+            alert("You WON the game with a total of " + playerScore + " winning rounds VS the CPU " + computerScore + " winning rounds!");
+        }else if(playerScore < computerScore){
+            alert("CPU WON the game with a total of " + computerScore + " winning rounds VS yours " + playerScore + " winning rounds.")
         }
     }

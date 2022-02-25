866439b32e9a0d3c3c749c23abc30df9c9d8af25
hugompt <hugomsfh@hotmail.com>
2022-02-25 14:43:14 +0100

    CHANGELOG
    - Added score to both player and CPU
    - Added round history

--- main.js
+++ main.js
@@ -4,6 +4,11 @@
 let playerScore = 0;
 let computerScore = 0;
 let roundWinner = " ";
+const roundsHistoric = document.querySelector("#roundsHist");
+const playerScoreText = document.querySelector(".playerScore");
+const cpuScoreText = document.querySelector(".cpuScore");
+
+roundsHistoric.setAttribute('style', 'white-space: pre;')
 
 //Function for randomly choosing a computer play between 3 possible options, Rock, Paper, Scissors;
     function computerPlay (){
@@ -16,21 +21,33 @@ let roundWinner = " ";
     function playRound (playerSelection, computerSelection){
         playerSelection = playerSelection.toUpperCase();
         computerSelection = computerSelection.toUpperCase();
+        roundsHistoric.textContent +="\n-----------------------------------------------\n"
         if(playerSelection === computerSelection){
-            alert("DRAW! Close but not enough! Both you and the CPU picked " + 
-                playerSelection);
+            roundsHistoric.textContent += "DRAW - Both you and the CPU picked " + 
+            playerSelection +"!";
+            // alert("DRAW! Close but not enough! Both you and the CPU picked " + 
+            //     playerSelection);
             return "DRAW"
         }else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"
                     || playerSelection === "PAPER" && computerSelection === "ROCK" 
                     || playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-                        alert("YOU WIN!!! Your " + playerSelection + " beats CPU's " + 
-                                computerSelection +"!");
+                        roundsHistoric.textContent += "WON - Your " + playerSelection + " beats CPU's " + 
+                        computerSelection +"!";
+                        playerScore++;
+                        playerScoreText.textContent = playerScoreText.textContent.replace(/.$/,playerScore);
+                        console.log(playerScore);
+                        // alert("YOU WIN!!! Your " + playerSelection + " beats CPU's " + 
+                        //         computerSelection +"!");
                         return "HUMAN"
         }else if (playerSelection === "ROCK" && computerSelection === "PAPER" 
                     || playerSelection === "PAPER" && computerSelection === "SCISSORS" 
                     || playerSelection === "SCISSORS" && computerSelection === "ROCK"){
-                        alert("YOU LOSE! The CPU's " + computerSelection + " beats your " + 
-                                playerSelection + ", try again!");
+                        roundsHistoric.textContent += "LOST - The CPU's " + computerSelection + " beats your " + 
+                        playerSelection + "!";
+                        computerScore++;
+                        cpuScoreText.textContent = cpuScoreText.textContent.replace(/.$/,computerScore);
+                        // alert("YOU LOSE! The CPU's " + computerSelection + " beats your " + 
+                        //         playerSelection + ", try again!");
                         return "CPU"
         }else{
             alert("WARNING! Please enter a valid response: 'Rock' or 'Paper'" +
@@ -62,13 +79,11 @@ let roundWinner = " ";
     //     }
     // }
 
-    const playerRock = document.getElementById("rock");
-
     window.onload = function(){ 
         document.getElementById("rock").onclick = function(){playRound(document.getElementById("rock").getAttribute('id'),computerPlay());};
         document.getElementById("paper").onclick = function(){playRound(document.getElementById("paper").getAttribute('id'),computerPlay());};
-        document.getElementById("scissors").onclick = function(){playRound(document.getElementById("scissors").getAttribute('id'),computerPlay());};
-        
+        document.getElementById("scissors").onclick = function(){playRound(document.getElementById("scissors").getAttribute('id'),computerPlay());};       
+     
         // Swal.fire({
         //     title: 'Error!',
         //     text: 'Do you want to continue',
